7cd4e2365bf540dafee8a20e3f341d10af3983d2
Michael Duprey <mike.a.duprey@gmail.com>
2019-01-27 18:45:33 -0500

    Improved documentation

--- example.R
+++ example.R
@@ -1,17 +1,13 @@
-# Author: Michael Duprey
-# Date: May 13, 2018
-
-# Example usage -----------------------------------------------------------
 source("./innerProduct.R")
 source("./gramSchmidt.R")
 
-# Integral inner product
+# Calculate the integral inner product for vectors u, v
 u <- c(3, .5, 6) 
 v <- c(1, 0, 2)
 s <- u %i% v
 s
 
-# Gram-Schmidt process
+# Invoke Gram-Schmidt process for three vectors from the R^3 inner product space
 V <- list(c(1, 3, 2), c(2, 0, 0), c(1, 3, 0))
 E <- gramSchmidt(V)
 E
